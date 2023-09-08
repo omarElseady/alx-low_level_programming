be4f7689ba8cbd0089fea33f5646269f7313619b
Omar <you@example.com>
2023-09-07 19:25:24 -0700

    My second commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  * main -Entry point
+ *   *
+ *    * Description :A C program that prints with prinf funcyion.
+ *     *
+ *      * return: always 0(Succss)
+ *       */
+
+int main(void)
+{
+		printf("size of a" ,sizof());
+		 printf("size of a" ,sizof());
+		  printf("size of a" ,sizof());
+		   printf("size of a" ,sizof(long long ));
+		    printf("size of a" ,sizof(float));
+			return (0);
