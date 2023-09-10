a8453a881c5fa64843a5ec3e27a04ffce4423fad
Omar <you@example.com>
2023-09-10 01:33:12 -0700

    My second commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *
+ *Description: write lower and upper alphabet
+ *
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+	char o = 'A';
+
+	while (c <= 'z')
+	{
+		if (c == 'q' || c == 'e')
+		{
+			c++;
+			continue;
+		}
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
