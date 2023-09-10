5e0558454401d2e877b6a5ff606a2725ba9803f2
Omar <you@example.com>
2023-09-10 01:49:28 -0700

    omar

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *
+ *Description: digit from 0 to 9
+ *
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%i", a);
+		a++;
+	}
+
+
+	putchar('\n');
+
+	return (0);
+}
