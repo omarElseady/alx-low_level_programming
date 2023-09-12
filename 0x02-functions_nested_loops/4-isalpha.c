181d64714086e3b3eb7ae9fbefa1092c417909fc
Omar <you@example.com>
2023-09-12 16:58:04 -0700

    My second commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,12 +10,13 @@
 
 int _islower(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
-	else
+	if (c >= 65&& c <= 90)
 	{
-		return (0);
-	}
+		return(1);
+	}	
+
 }
