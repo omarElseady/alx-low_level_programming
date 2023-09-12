1002187b0ead33567ec0dcd8af00f8684f918e76
Omar <you@example.com>
2023-09-12 16:26:14 -0700

    My second commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - omar maher
+ */
+
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		int ch = 'a';
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			putchar(ch);
+		}
+		putchar('\n');
+	}
+
+}
