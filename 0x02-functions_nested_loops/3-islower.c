443b96452242d2141407c2fad597961825182cd1
Omar <you@example.com>
2023-09-12 16:46:52 -0700

    My second commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - omar maher
+ *
+ * @c: check input
+ *
+ * Return: return 1 or 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
