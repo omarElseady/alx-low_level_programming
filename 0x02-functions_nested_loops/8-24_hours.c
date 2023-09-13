a4da037eade00729886f7361250366de0d7314d7
Omar <you@example.com>
2023-09-13 16:51:20 -0700

    My second commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,10 @@
 void jack_bauer(void)
 {
 	int i = 0, j = 0;
-	while (i < 24)
+
+	while (i <= 23)
 	{
-		while ( j < 60)
+		while (j <= 59)
 		{
 			_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
