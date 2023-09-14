e6e6c999a32f803b80d03c5b02769dbcae4eaf32
Omar <you@example.com>
2023-09-13 18:07:21 -0700

    My second commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * add - omar maher
+ *
+ * @n: check input
+ *
+ * Return: return 1 or
+ */
+
+int add(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (int i = 0; i < 98; i--)
+		{
+			printf("%i, ", i);
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 98; i++)
+		{
+			printf("%i, ", i);
+		}
+	}
+	printf("98\n");
+	return (a);
+}
