5630319b085a18442b88175316eea35a089d32c8
Omar <you@example.com>
2023-09-15 17:29:05 -0700

    My second commit

---
+++ 0x02-functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - omar maher
+ *
+ * @c: check input
+ *
+ * Return: return 1 or 0
+ */
+
+int _isupper(int c);
+
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
