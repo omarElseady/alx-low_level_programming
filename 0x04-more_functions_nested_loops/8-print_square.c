86115ca00401e56fc98bd84751aed07377659d19
Omar <you@example.com>
2023-09-16 16:48:20 -0700

    My second commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,7 +3,7 @@
 /**
  * print_square - omar maher
  *
- * @n: oma
+ * @size: oma
  * Return: return 1 or 0
  */
 
@@ -12,15 +12,15 @@ void print_square(int size)
 	int i, j;
 
 
-	if (n <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
