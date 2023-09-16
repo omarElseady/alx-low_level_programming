9b470a18b7f242133c3d337881e6857863125557
Omar <you@example.com>
2023-09-15 17:55:07 -0700

    My second commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - omar maher
+ *
+ * @c: check input
+ *
+ * Return: return 1 or 0
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
