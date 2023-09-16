102580d475b0cb466e4ecbb05553aa0f72ee511e
Omar <you@example.com>
2023-09-16 16:28:33 -0700

    My second commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - omar maher
+ *
+ * @n: oma
+ * Return: return 1 or 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
