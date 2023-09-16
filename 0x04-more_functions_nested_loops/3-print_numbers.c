4c89ae721589c3457148ded8ceaf955de2105820
Omar <you@example.com>
2023-09-15 18:05:39 -0700

    My second commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - omar maher
+ *
+ * Return: return 1 or 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+
+}
