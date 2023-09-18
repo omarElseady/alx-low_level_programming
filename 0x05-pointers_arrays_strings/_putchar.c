2a8e80d0788e2df5f142bd2641b8d2d119685488
Omar <you@example.com>
2023-09-18 16:52:06 -0700

    My second commit

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *  _putchar - writes the character c to stdout
+ *   @c: The character to print
+ *  
+ *     Rturn: On success 1 
+ *   On error , -1 isreturned, and errno is st appropriatly,
+ */
+int _putchar(char c)
+{
+			return (write(1, &c, 1));
+}
