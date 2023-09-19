06878a01fc490db1184e2f07a62bbbe954dd9e14
Omar <you@example.com>
2023-09-18 17:06:18 -0700

    My second commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - omar maher
+ *
+ * @a: om
+ * @b: int
+ * Return: return 1 or 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int o;
+
+	o = *a;
+	*a = *b;
+	*b = o;
+
+}
