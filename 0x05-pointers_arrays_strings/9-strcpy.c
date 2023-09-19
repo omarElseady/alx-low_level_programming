29901cdbe0b1ad02be18a7c2bed8d63a56e1548e
Omar <you@example.com>
2023-09-18 18:26:10 -0700

    My second commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - omar maher
+ *
+ * @dest: om
+ *
+ * @src: pp
+ * Return: return 1 or 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0')
+
+	return (dest);
+}
