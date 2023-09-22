46be687984bc7dc4b5aac0805347942022ff22f7
Omar <you@example.com>
2023-09-21 17:08:41 -0700

    My second commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  * reverse_array - omar maher
+ *   *
+ *    * @a: om
+ *     *
+ *      * @n :omj
+ *       *
+ *        * Return: return 1 or 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, t;
+
+	for (i = 0, j = (n - 1); i < j; i++, j++)
+	{
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
