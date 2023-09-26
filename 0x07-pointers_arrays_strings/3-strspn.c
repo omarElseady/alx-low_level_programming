5776c3bbf7c07a7483087e7a2d4d75b10eb78f16
Omar <you@example.com>
2023-09-26 01:43:52 -0700

    My second commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - omar maher
+ *
+ * @s: om
+ *
+ * @accept: mm
+ *
+ * Return: return 1 or 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accept[j] == '\0')
+			{
+				return (i);
+			}
+		}
+	}
+
+	return (i);
+}
