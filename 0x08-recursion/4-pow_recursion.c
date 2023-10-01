32d026ff0a63bcd42afca9b2d82d559a6829da46
Omar <you@example.com>
2023-10-01 16:35:54 -0700

    My second commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  * _pow_recursion - omar maher
+ *   *
+ *    * @x: om
+ *     *
+ *      * @y: op
+ *       * Return: return 1 or 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1))
+
+}
