3125e22ce170b0b15d4baafedaf8c9e158280f63
Omar <you@example.com>
2023-10-01 17:07:26 -0700

    My second commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  * _sqrt_recursion- omar maher
+ *   *
+ *    * @n: om
+ *     * @val: opp
+ *      * Return: return 1 or 0
+ */
+
+int square(int n, int val);
+int _sqrt_recursion(int n);
+{
+	return (square(n, 1));
+}
+/**
+  * square - omat
+  *  * @n: iny
+  *   * @val: oomo
+  *    * Return: int
+  */
+
+int square(int n, int val)
+{
+	if (val * val == n)
+		return (val);
+	else if (val * val < n)
+		return (square(n, val + 1));
+	else
+		return (-1);
+}
