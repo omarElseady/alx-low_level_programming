06e715755f0f72d49e71e5c199b2efeca958fcec
Omar <you@example.com>
2023-10-03 15:39:29 -0700

    My second commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  * *create_array- omar maher
+ *   *
+ *    * @size: om
+ *     *
+ *      * @c: okvk
+ *       * Return:  0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char 8n = malloc(size)
+
+		if (size == 0 || n == 0)
+			return (0);
+
+	while (size--)
+		n[size] = c;
+
+	return (n);
+}
