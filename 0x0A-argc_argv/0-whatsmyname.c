2559565a9c84a825e19be605e9703b5b5ab24c07
Omar <you@example.com>
2023-10-03 14:55:25 -0700

    My second commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  * main- omar maher
+ *   *
+ *    * @argc: om
+ *     *
+ *      * @argv: okvk
+ *       * Return:  0
+ */
+
+int main(int argc, int *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+
