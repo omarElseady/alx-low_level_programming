9b80a7ee505b1fe5f3dc60bc7cd7ffb8311d7966
Omar <you@example.com>
2023-10-04 14:27:49 -0700

    My second commit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+ /**
+  *  * free_grid- omar maher
+  *   *
+  *    * @grid: om
+  *     *
+  *      * @height: odvfv
+  *       * Return: po 0
+  */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
