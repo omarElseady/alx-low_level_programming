c27b2f21150d9e2ea9febde94305d917bd05a25c
Omar <you@example.com>
2023-10-04 15:14:17 -0700

    My second commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -36,16 +36,16 @@ char *argstostr(int ac, char **av)
 	for (; i < ac; i++, nc++)
 		nc += _strlen(av[i]);
 
-	s = malloc(sizof(char) * nc + 1);
+	s = malloc(sizeof(char) * nc + 1);
 	if (s == 0)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++; cmpt++)
+		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
 			s[cmpt] = av[i][j];
 
-		c[cmpt] =  '\n';
+		s[cmpt] =  '\n';
 		cmpt++;
 	}
 	s[cmpt] = '\0';
