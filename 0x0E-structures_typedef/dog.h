1c76ab07fff2a9205a67a65cadbe21a533c4d83f
Omar <you@example.com>
2023-10-10 15:54:54 -0700

    My second commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _SDOG_H
+
+/**
+ *  * struct dog - a dog struct
+ *   * @name: fddf
+ *    * @age: fkdlrfc
+ *     * @owner: fdjkf
+ *      *
+ *       *Description: fjkdgfkf
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
